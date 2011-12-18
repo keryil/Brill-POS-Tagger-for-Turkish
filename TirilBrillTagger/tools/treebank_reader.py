e7775c4419c307cf811583b4964b342e034ce0af
Kerem <Kerem@Kerem-PC>
2011-12-18 16:19:20 +0200

    * Added various "ignore"s

---
+++ TirilBrillTagger/tools/treebank_reader.py
@@ -0,0 +1,17 @@
+'''
+Created on 17 Ara 2011
+
+@author: Kerem
+'''
+
+class MyClass(nltk.corpus.reader.):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
