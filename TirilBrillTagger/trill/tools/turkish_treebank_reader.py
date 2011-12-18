b588aa95898febd16e40ef2b3ffbd69d7bcb6110
Kerem <Kerem@Kerem-PC>
2011-12-18 19:08:00 +0200

    * Played around  * Found XMLCorpusReader is the way to go  * Created new corpus reader class inheriting from XMLCorpusReader

--- TirilBrillTagger/trill/tools/turkish_treebank_reader.py
+++ TirilBrillTagger/trill/tools/turkish_treebank_reader.py
@@ -3,15 +3,31 @@ Created on 17 Ara 2011
 
 @author: Kerem
 '''
+#from nltk.corpus.reader.api import SyntaxCorpusReader
+from nltk.corpus.util import LazyCorpusLoader
+from nltk.corpus.reader import XMLCorpusReader
+from xml.etree.cElementTree import ParseError
 
-class MyClass(nltk.corpus.reader.):
+class TurkishTreebankCorpusReader(XMLCorpusReader):
     '''
     classdocs
     '''
 
-
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
-        
+    def abc(self):
+        pass
+      
+if __name__ == '__main__':
+    import os
+    print os.getcwd()
+    fileids = [line.rstrip() for line in open("../filelist.txt")]
+    print fileids
+    turkish_treebank = LazyCorpusLoader(name="turkish_treebank", 
+                                        reader_cls=TurkishTreebankCorpusReader,
+                                        fileids=fileids)
+#    print turkish_treebank.words()
+    for file in turkish_treebank.fileids():
+        try:
+#            continue
+            print turkish_treebank.words(file)
+        except ParseError, e:
+            print "ERROR: file, e"
