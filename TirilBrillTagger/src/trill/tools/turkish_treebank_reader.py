c39aaa1e6ac58a08115b1d1ea85e031bdbe2a9e7
Kerem <Kerem@Kerem-PC>
2012-01-17 19:49:02 +0200

    Changes for cross-platform. Added YAML library.

--- TirilBrillTagger/src/trill/tools/turkish_treebank_reader.py
+++ TirilBrillTagger/src/trill/tools/turkish_treebank_reader.py
@@ -8,12 +8,14 @@ import nltk
 from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.reader import XMLCorpusReader
 from xml.etree.cElementTree import ParseError
-
+win_data_dir = "C:\\Users\\Kerem\\nltk_data"
+nix_data_dir = "/home/kerem/nltk_data"
 class TurkishTreebankCorpusReader(XMLCorpusReader):
     '''
     classdocs
     '''
-
+    
+    
     def sents(self, fileid):
         tree = self.xml(fileid)
         sents_xml = tree.findall("S")
@@ -25,7 +27,14 @@ class TurkishTreebankCorpusReader(XMLCorpusReader):
 if __name__ == '__main__':
     import os
     print os.getcwd()
-    fileids = [file for file in os.listdir("../../../res/treebank/")]
+    fileids = None
+    treebank_path = None
+    if os.name == "nt":
+        treebank_path = os.path.join(win_data_dir,"corpora", "turkish_treebank")
+    else:
+        treebank_path = os.path.join(nix_data_dir,"corpora", "turkish_treebank")
+    print treebank_path
+    fileids = [file for file in os.listdir(treebank_path)]
 #    print fileids
     turkish_treebank = LazyCorpusLoader(name="turkish_treebank", 
                                         reader_cls=TurkishTreebankCorpusReader,
@@ -33,6 +42,6 @@ if __name__ == '__main__':
 #    print turkish_treebank.words()
     for file in turkish_treebank.fileids():
         try:
-            print turkish_treebank.sents(file)
+            turkish_treebank.sents(file)
         except ParseError, e:
             print "ERROR: %s, %s" % (file, e)
