a3135582bcd06854508796b4675e75ce634c076e
Kerem Eryılmaz <keryilmaz@gmail.com>
2012-01-18 16:42:28 +0200

    Fixed fix_treebank further, implemented tagged word and sentence output, and parsing for Major/Minor POS.

--- TirilBrillTagger/src/trill/tools/fix_treebank.py
+++ TirilBrillTagger/src/trill/tools/fix_treebank.py
@@ -16,17 +16,27 @@ nix_data_dir = "/home/kerem/nltk_data/corpora/turkish_treebank"
 import re
 #apos_re = re.compile("IG\d*=\'\[\(\d+,\"([^\)]*?\'[^\)]*?)\"\)\]\'")
 #apos_re = re.compile("IG\d*=\'\[(\(([^)(]+?\'[^)(]+?)\)+)\]\'")
-apos_re = re.compile("IG\d*=\'\[([^\[\]]*?\'[^\[\]]*?)\]\'")
-
+apos_re = re.compile("IG\d*=\'\[[^\[\]]*?\"(\')\"[^\[\]]*?\]\'")
+apos_re = re.compile("\(\d+,\"([^\"]+?'[^\"]+?)\"\)")
+#apos_re = re.compile("\(\d+,\"([^\"]+?'[^\"]+?)\"\)")
+#apos_re = re.compile("\'+Punc")
 
 def change(file_path):
     content = open(file_path).readlines()
     new_lines = []
     for line in content:
         new_line = line
+        if "'+Punc" in line:
+            print "Replacing '+Punc"
+            new_line = line.replace("'+Punc","&apos;+Punc")
+        if '"+Punc' in new_line:
+            print "Replacing \"+Punc"
+            new_line = new_line.replace('"+Punc',"&quot;+Punc")
         match = apos_re.search(new_line)
         while match:
+            print "Replacing %s" % match.group(1)
             new_line = new_line.replace(match.group(1), match.group(1).replace("'", "&apos;"))
+#            new_line = new_line.replace(match.group(1), match.group(1).replace('"', "&quot;"))
             match = apos_re.search(new_line)
         new_lines.append(new_line)
     f = open(file_path, "w")
@@ -55,8 +65,14 @@ if __name__ == '__main__':
 #    print new_line
 #    
 #    exit()
-    
     if os.name != "nt":
         data = nix_data_dir
+    
+    
+#    apos_re = re.compile("\(\d+,\"([^\"]+?'[^\"]+?)\"\)")
+#    line = """<W IX="1" LEM="" MORPH=" " IG='[(1,"Yaralılardan_Burak_Altındağ'ın_abisi_Vecdi_Altındağ+Noun+Prop+A3sg+Pnon+Nom")]' REL="[12,2,(SUBJECT)]" ORG_IG1='[(1,"yara+Noun+A3sg+Pnon+Nom")(2,"Adj+With")(3,"Noun+Zero+A3pl+Pnon+Abl")]' ORG_IG2='[(1,"Burak+Noun+Prop+A3sg+Pnon+Nom")]' ORG_IG3='[(1,"Altındağ+Noun+Prop+A3sg+Pnon+Gen")]' ORG_IG4='[(1,"abi+Noun+A3sg+P3sg+Nom")]' ORG_IG5='[(1,"Vecdi+Noun+Prop+A3sg+Pnon+Nom")]' ORG_IG6='[(1,"Altındağ+Noun+Prop+A3sg+Pnon+Nom")]'> Yaralılardan_Burak_Altındağ'ın_abisi_Vecdi_Altındağ </W>"""
+#    m = apos_re.search(line)
+#    print m.groups()
+#    exit()
     for file in os.listdir(data):
         change(os.path.join(data,file))
--- TirilBrillTagger/src/trill/tools/turkish_treebank_reader.py
+++ TirilBrillTagger/src/trill/tools/turkish_treebank_reader.py
@@ -4,49 +4,164 @@ Created on 17 Ara 2011
 @author: Kerem
 '''
 #from nltk.corpus.reader.api import SyntaxCorpusReader
-import nltk
+import nltk, re
 from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.reader import XMLCorpusReader
 from xml.etree.cElementTree import ParseError
 from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import tostring
 win_data_dir = "C:\\Users\\Kerem\\nltk_data"
 nix_data_dir = "/home/kerem/nltk_data"
+#tag_re = re.compile("\[\(.*?\)*?\(\d,\"(.+?)\"\)\]")
+#tag_re = re.compile("\[(\([^\(\)]*?\))*?\(\d+,\"(.+?)\"\)\]")
+#tag_re = re.compile("\[(\(\d+,\"[^\"]+?\"\))*?\(\d+,\"([^\"]+?)\"\)\]")
+tag_re = re.compile("\[(\(\d+,\".+?\"\))*\(\d+,\"(.+?)\"\)\]")
+MAJOR_POS = ["Noun", "Adj", "Adv", "Verb", "Pron", "Conj", "Det", "Postp", \
+             "Ques", "Interj", "Num", "Dup", "Punc"]
+MINOR_POS = ["Inf", "PastPart", "FutPart", "Prop", "Zero", "PresPart", "DemonsP", \
+             "QuesP", "ReflexP", "QuantP", "Card", "Ord", "Percent", "Range", "Real", \
+             "Ratio", "Distrib", "Time"] 
 class TurkishTreebankCorpusReader(XMLCorpusReader):
     '''
     classdocs
     '''
     
+    def words(self, fileids):
+        words = []
+        for fileid in fileids:
+            tree = self.xml(fileid)
+    #        print tree
+            words_xml = tree.findall("S/W")
+            for xml in words_xml:
+    #            print xml
+                words.append(xml.text.rstrip().lstrip())
+                
+        return words
     
-    def sents(self, fileid):
-        tree = self.xml(fileid)
-        sents_xml = tree.findall("S")
+    def tagged_words(self, fileids):
+        words = []
+        for id in fileids:
+            print "File: %s" % id
+            tree = self.xml(id)
+            words_xml = tree.findall("S/W")
+            words = []
+            for word in words_xml:
+#                print "\n%s: %s" % (word.get("IX"), word.text)
+#                print tostring(word, encoding="utf-8").rstrip()
+#                print word.get("IG")
+                text = word.text.rstrip().lstrip()
+                ig = word.get("IG")
+                print ig
+                index, tag = tag_re.search(ig).groups(-1)
+#                print tag
+                parts = tag.split("+")
+                relevant_parts = []
+#                print parts
+                for part in parts:
+                    if part not in MAJOR_POS:
+                        if relevant_parts == []:
+                            continue
+                        else:
+                            if part not in MINOR_POS:
+                                break
+                            else:
+                                print "MINOR: %s" % part
+                                relevant_parts.append(part)
+                    else:
+                        print "MAJOR: %s" % part
+                        relevant_parts.append(part)
+#                first_portion = tag.split("+")[0]
+#                if text.startswith(first_portion):
+#                    tag = tag.replace("%s+" % first_portion, "")
+#                print tag
+#                print text, tag
+                tag = "+".join(relevant_parts)
+                print text, tag
+#                print "\n"
+                words.append((text, tag))
+        return words
+    
+    def tagged_sents(self, fileids):
         sents = []
-        for xml in sents_xml:
-            for child in xml:
-                sents.append(child.text)
-      
+        for id in fileids:
+#            print id
+            tree = self.xml(id)
+            sents_xml = tree.findall("S")
+            for sent_xml in sents_xml:
+                sent = []
+                for word in sent_xml:
+                    text = word.text.rstrip().lstrip()
+                    ig = word.get("IG")
+#                    print ig
+                    index, tag = tag_re.search(ig).groups(-1)
+                    parts = tag.split("+")
+                    relevant_parts = []
+                    for part in parts:
+                        if part not in MAJOR_POS:
+                            if relevant_parts == []:
+                                continue
+                            else:
+                                if part not in MINOR_POS:
+                                    break
+                                else:
+#                                    print "MINOR: %s" % part
+                                    relevant_parts.append(part)
+                        else:
+#                            print "MAJOR: %s" % part
+                            relevant_parts.append(part)
+    #                first_portion = tag.split("+")[0]
+    #                if text.startswith(first_portion):
+    #                    tag = tag.replace("%s+" % first_portion, "")
+    #                print tag
+    #                print text, tag
+                    tag = "+".join(relevant_parts)
+#                    print text, tag
+    #                print "\n"
+                    sent.append((text, tag))
+                sents.append(sent)
+#                print "Appended %s" % sent
+        return sents
+    
+    def sents(self, fileids):
+        sents = []
+        for fileid in fileids:
+            tree = self.xml(fileid)
+            sents_xml = tree.findall("S")
+            for xml in sents_xml:
+                sent = []
+                for child in xml:
+                    sent.append(child.text.rstrip().lstrip())
+                sents.append(sent)
+        return sents
+    
+    
 if __name__ == '__main__':
     import os
     print os.getcwd()
     fileids = None
     treebank_path = None
     if os.name == "nt":
-        treebank_path = os.path.join(win_data_dir,"corpora", "turkish_treebank")
+        treebank_path = os.path.join(win_data_dir, "corpora", "turkish_treebank")
     else:
-        treebank_path = os.path.join(nix_data_dir,"corpora", "turkish_treebank")
+        treebank_path = os.path.join(nix_data_dir, "corpora", "turkish_treebank")
     print treebank_path
     fileids = [file for file in os.listdir(treebank_path)]
 #    print fileids
-    turkish_treebank = LazyCorpusLoader(name="turkish_treebank", 
+    turkish_treebank = LazyCorpusLoader(name="turkish_treebank",
                                         reader_cls=TurkishTreebankCorpusReader,
                                         fileids=fileids)
+    sents = turkish_treebank.tagged_sents(fileids)
+    print len(sents)
+#    for sent in sents:
+#        print sent
 #    print turkish_treebank.words()
-    for file in turkish_treebank.fileids():
-        try:
-            turkish_treebank.sents(file)
-        except ParseError, e:
-            print file
-            tree = ElementTree()
-            tree.parse(open(os.path.join(treebank_path, file)))
-            print tree
+#    for file in turkish_treebank.fileids():
+#        try:
+##            print turkish_treebank.sents(file)
+#            print turkish_treebank.tagged_words(file)
+#        except ParseError, e:
+#            print file
+#            tree = ElementTree()
+#            tree.parse(open(os.path.join(treebank_path, file)))
+#            print tree
 #            print "ERROR: %s, %s" % (file, e)
